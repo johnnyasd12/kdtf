a10ef5b2ed43e9b4b8b5cd2ec7dc804a8bee70d2
johnnyasd12 <johnnnyasd12@gmail.com>
2018-11-01 17:10:36 +0800

    developing other dataset, and testing

--- data.py
+++ data.py
@@ -1,5 +1,8 @@
 from tensorflow.examples.tutorials.mnist import input_data
+from keras.datasets import mnist
 
+import numpy as np
+# from utils import *
 
 class Dataset:
     def __init__(self, args):
@@ -17,3 +20,49 @@ class Dataset:
 
     def get_validation_data(self):
         return (self.mnist.validation.images, self.mnist.validation.labels)
+
+
+class MnistKeras:
+    def __init__(self):
+        print('fetching mnist from Keras...')
+        (X_train,y_train),(self.X_test,self.y_test) = mnist.load_data()
+        print('finished.')
+        
+        
+
+
+def check_obj(obj_str):
+    obj = eval(obj_str)
+    obj_type = type(obj)
+    print(obj_str,obj_type,end=' ')
+    if obj_type == np.ndarray:
+        print(obj.shape)
+    else:
+        try:
+            iterator = iter(obj)
+        except TypeError:
+            # not iterable
+            print()
+        else:
+            # iterable
+            print(len(obj))
+
+if __name__ == '__main__':
+    # mnist_keras = MnistKeras()
+
+    # test tensorflow
+    mnist = input_data.read_data_sets("./",one_hot=True)
+    n_samples = mnist.train._num_examples
+    batch_size = 32
+    X_train = mnist.train.images
+    y_train = mnist.train.labels
+    X_train_batch,y_train_batch = mnist.train.next_batch(batch_size)
+    X_test = mnist.test.images
+    y_test = mnist.test.labels
+    check_obj('X_train_batch')
+    check_obj('y_train_batch')
+    
+
+
+
+
